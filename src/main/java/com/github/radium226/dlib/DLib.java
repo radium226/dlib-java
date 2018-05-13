4cb9457b0005d4c2f46e9b1cf60c12c4ff5f4dcc
radium <r@di.um>
2018-05-13 22:55:28 +0200

    Some adjustments

--- src/main/java/com/github/radium226/dlib/DLib.java
+++ src/main/java/com/github/radium226/dlib/DLib.java
@@ -6,8 +6,30 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class DLib {
+
+    public static boolean loadLibraryFromTargetFolder() {
+        Path libraryFilePath = Paths.get(System.getProperty("user.dir"), "target/generated-resources/swig/libdlib-java.so");
+        if (Files.exists(libraryFilePath)) {
+            System.load(libraryFilePath.toString());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void loadLibrary() {
+        System.loadLibrary("dlib");
+        if (loadLibraryFromTargetFolder()) {
+
+        } else if (loadLibraryFromJarFile()) {
+
+        } else {
+            throw new UnsatisfiedLinkError("dlib-java");
+        }
+    }
 
     public static Path unzipLibrary() throws IOException {
         InputStream inputStream = DLib.class.getClassLoader().getResourceAsStream("libdlib-java.so");
@@ -17,15 +39,20 @@ public class DLib {
         return libraryFilePath;
     }
 
-    static {
+    public static boolean loadLibraryFromJarFile() {
         try {
             Path libraryFilePath = unzipLibrary();
             System.out.println(libraryFilePath);
             System.load(libraryFilePath.toString());
+            return true;
         } catch (IOException e) {
-            e.printStackTrace(System.err);
-            throw new UnsatisfiedLinkError("libdlib-java");
+            e.printStackTrace();
+            return false;
         }
+    }
+
+    static {
+        loadLibrary();
     }
 
     public static void main( String[] args ) {
