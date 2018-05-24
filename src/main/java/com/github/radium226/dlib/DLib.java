59e82a7563b719969783d91b527650e072a2ef2a
radium <r@di.um>
2018-05-24 16:08:47 +0200

    Add OpenCV class to load native libraries

--- src/main/java/com/github/radium226/dlib/DLib.java
+++ src/main/java/com/github/radium226/dlib/DLib.java
@@ -1,13 +1,15 @@
 package com.github.radium226.dlib;
 
 import com.github.radium226.commons.Libraries;
+import com.github.radium226.opencv.OpenCV;
 
 import java.util.Arrays;
 
 public class DLib {
 
     public static void loadLibraries() {
-        for (String libraryName : Arrays.asList("dlib", "dlib-java", "opencv_java341")) {
+        OpenCV.loadLibraries();
+        for (String libraryName : Arrays.asList("dlib", "dlib-java")) {
             Libraries.loadLibrary(libraryName);
         }
     }
---
+++ src/main/java/com/github/radium226/opencv/OpenCV.java
@@ -0,0 +1,12 @@
+package com.github.radium226.opencv;
+
+import com.github.radium226.commons.Libraries;
+
+import java.util.Arrays;
+
+public class OpenCV {
+
+    public static void loadLibraries() {
+        Libraries.loadLibrary("opencv_java341");
+    }
+}
